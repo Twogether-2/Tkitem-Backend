84c01392b9e9717a6e4a598e2418dc2c92bac9a1
HideOnCodec <tlsdmsgp33@naver.com>
2025-08-18 12:21:32 +0900

    feat: 이메일 인증 코드 전송 기능 구현

--- src/main/java/tkitem/backend/domain/member/service/MailService.java
+++
@@ -1,8 +0,0 @@
-package tkitem.backend.domain.member.service;
-
-public interface MailService {
-	// 이메일 전송
-	void sendEmail(String email);
-	// 이메일 인증번호 확인
-	boolean checkEmailVerificationCode(String email, String code);
-}
