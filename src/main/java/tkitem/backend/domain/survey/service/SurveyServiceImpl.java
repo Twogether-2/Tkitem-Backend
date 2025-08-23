5ae2b118a0c81e1a0481d5a645b84def7606206a
HideOnCodec <tlsdmsgp33@naver.com>
2025-08-23 19:45:27 +0900

    feat: 취향 설문 조사 문항 가져오기 API 구현

---
+++ src/main/java/tkitem/backend/domain/survey/service/SurveyServiceImpl.java
@@ -0,0 +1,47 @@
+package tkitem.backend.domain.survey.service;
+
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import tkitem.backend.domain.member.vo.Member;
+import tkitem.backend.domain.survey.mapper.SurveyMapper;
+import tkitem.backend.domain.survey.vo.Age;
+import tkitem.backend.domain.survey.vo.Survey;
+import tkitem.backend.domain.survey.vo.SurveyQuestion;
+
+@Slf4j
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class SurveyServiceImpl implements SurveyService{
+	private final SurveyMapper surveyMapper;
+
+	@Override
+	public List<Survey> getSurveyQuestions(Member member) {
+		log.info("[SurveyService] getSurveyQuestions");
+		List<Survey> surveyList = surveyMapper.selectRandomSurveys();
+
+		// 회원 연령대 매칭
+		// 생일 문자열로 나이 계산(만 나이 기준)
+		LocalDate birthDate = LocalDate.parse(member.getBirthday());
+		int age = Period.between(birthDate, LocalDate.now()).getYears();
+		String ageType = age < 45 ? Age.YOUNG.name() : Age.OLD.name();
+
+		for(Survey survey : surveyList){
+			List<SurveyQuestion> surveyQuestionList = surveyMapper.selectSurveyQuestionBySurveyId(
+				survey.getSurveyId(),
+				ageType,
+				member.getGender()
+			);
+			survey.setQuestions(surveyQuestionList);
+		}
+
+		return surveyList;
+	}
+}
--- src/main/java/tkitem/backend/domain/survey/vo/Age.java
+++ src/main/java/tkitem/backend/domain/survey/vo/Age.java
@@ -1,8 +1,5 @@
 package tkitem.backend.domain.survey.vo;
 
-import lombok.ToString;
-
-@ToString
 public enum Age {
 	YOUNG, OLD
 }
