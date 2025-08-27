61732386e0ed68579bc277e4681daed88f8820e6
HideOnCodec <tlsdmsgp33@naver.com>
2025-08-27 15:29:22 +0900

    fix: log 출력 삭제

--- src/main/java/tkitem/backend/TkitemBackendApplication.java
+++ src/main/java/tkitem/backend/TkitemBackendApplication.java
@@ -1,22 +1,14 @@
 package tkitem.backend;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import lombok.extern.slf4j.Slf4j;
-
-@Slf4j
 @SpringBootApplication
 public class TkitemBackendApplication {
-
-	@Value("${cloud.aws.region.static}")
-	private static String db;
 
 	public static void main(String[] args) {
 
 		SpringApplication.run(TkitemBackendApplication.class, args);
-		log.info("db : {}", db);
 	}
 
 }
