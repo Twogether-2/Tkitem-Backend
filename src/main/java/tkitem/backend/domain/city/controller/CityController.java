efaa2333b31f5712affa122f279f2e4cf425f316
pch8349 <chan449@naver.com>
2025-09-01 09:05:12 +0900

    feat : 더미 데이터 제거

--- src/main/java/tkitem/backend/domain/city/controller/CityController.java
+++ src/main/java/tkitem/backend/domain/city/controller/CityController.java
@@ -21,20 +21,20 @@ public class CityController {
 
     @PostMapping("/init")
     public ResponseEntity<String> initializeCityData() {
-        try {
+//        try {
             // 1. ResourceLoader를 사용하여 클래스패스에서 파일을 찾습니다.
-            Resource resource = resourceLoader.getResource("classpath:dummy/도시.json");
+//            Resource resource = resourceLoader.getResource("classpath:dummy);
             
             // 2. 해당 파일의 절대 경로를 얻어옵니다.
-            String absolutePath = resource.getFile().getAbsolutePath();
+//            String absolutePath = resource.getFile().getAbsolutePath();
             
             // 3. Service에 파일 경로를 전달하여 데이터를 초기화합니다.
-            cityService.addCitiesFromJson(absolutePath);
+//            cityService.addCitiesFromJson(absolutePath);
             
             return ResponseEntity.ok("도시 데이터 초기화에 성공했습니다.");
-        } catch (IOException e) {
-            e.printStackTrace();
-            return ResponseEntity.internalServerError().body("도시 데이터 초기화 중 오류가 발생했습니다: " + e.getMessage());
-        }
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//            return ResponseEntity.internalServerError().body("도시 데이터 초기화 중 오류가 발생했습니다: " + e.getMessage());
+//        }
     }
 }
